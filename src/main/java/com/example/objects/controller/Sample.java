fe6b169fe439fb4519e0427da31c16eaf360c934
hyejinggu <118355536+hyejinggu@users.noreply.github.com>
2024-05-10 14:44:28 +0900

    create model

--- src/main/java/com/example/objects/controller/Sample.java
+++
@@ -1,5 +0,0 @@
-package com.example.objects.controller;
-
-public class Sample {
-
-}
---
+++ src/main/java/model/Bag.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Bag {
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
